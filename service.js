fa2ee5729d4225f3c76316b364182535deefa375
Yao <elites96105@gmail.com>
2021-12-09 21:36:38 +0800

    first commit

---
+++ service.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res, next){
+    res.json({msg: "hello world!"})
+});
+
+app.get('/test', function(req, res, next){
+    res.json({msg: "hello world test!!"})
+});
+
+var server = app.listen(3000, () => {
+    console.log('Node server is running..')
+})
